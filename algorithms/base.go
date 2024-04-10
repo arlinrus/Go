2e80bc3909e1e97dd4a79fb2ac315fac797dda5b
arlinrus <111064731+arlinrus@users.noreply.github.com>
2024-04-10 10:25:53 +0300

    Update base.go

--- algorithms/base.go
+++ algorithms/base.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	arr := createArr()
+	fmt.Println(arr[:10])
+
+	emptyArr := make([]int, len(arr))
+	copy(emptyArr, arr)
+	start := time.Now()
+	Bubblesort(emptyArr)
+	duration := time.Since(start)
+	fmt.Println("Время выполнения O(n^2): ", duration)
+
+}
+
+func createArr() (arr []int) {
+	sizeArr := 1000
+	arr := make([]int, sizeArr)
+
+	for i := 0; i < sizeArr; i++ {
+		arr[i] = rand.Intn(sizeArr)
+
+	}
+	return
+}
